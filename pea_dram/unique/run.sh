b5273c430fc8d8a822d998e5f8202dcc24ed6056
liuzhuoxuan <liuzhuoxuan17@mails.ucas.ac.cn>
2024-07-25 18:04:57 +0800

    rm redundant

--- pea_dram/unique/run.sh
+++ pea_dram/unique/run.sh
@@ -10,11 +10,6 @@ op_type=(full mixed)
 # prepare
 rm build/test_pmem
 rm build/CMakeCache.txt
-rm /mnt/mypmem1/liuzhuoxuan/pmem_pea.data
-rm /mnt/mypmem1/liuzhuoxuan/pmem_dash-16.data
-rm /mnt/mypmem1/liuzhuoxuan/pmem_dash-512.data
-rm /mnt/mypmem1/liuzhuoxuan/pmem_level.data
-rm /mnt/mypmem1/liuzhuoxuan/pmem_cceh.data
 
 # compile
 cmake -DCMAKE_BUILD_TYPE=Release -S . -B build
